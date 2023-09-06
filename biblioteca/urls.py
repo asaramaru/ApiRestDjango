8088ef6f210b5916ba21bbd6407117631705ab93
BRAYAN DANIEL ARENAS GIL <bdarenas1@misena.edu.co>
2023-09-06 16:50:30 -0500

    URL

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -22,7 +22,7 @@ from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('libros/', include('principal.urls')),
+    path('', include('principal.urls')),
     path('docs/', include_docs_urls(title="Documentacion de los libros")),
 ]
 
