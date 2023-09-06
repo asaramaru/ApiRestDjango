6dd2248fd4475cc1bdb15e5130275e9eab7fbf06
BRAYAN DANIEL ARENAS GIL <bdarenas1@misena.edu.co>
2023-09-05 22:14:28 -0500

    Database

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'biblioteca.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/biblioteca',
         conn_max_age=600,
     )
 }
