35a6bd2f7a09475ef139a317e4b2911c5e29e347
BRAYAN DANIEL ARENAS GIL <bdarenas1@misena.edu.co>
2023-09-05 22:06:12 -0500

    pillow

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-pip install dj-database-url django djangorestframework whitenoise psycopg2-binary coreapi gunicorn
+pip install dj-database-url django djangorestframework whitenoise psycopg2-binary coreapi gunicorn pillow
 
 python manage.py collectstatic --no-input
 python manage.py migrate
