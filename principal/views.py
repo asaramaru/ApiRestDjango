cf09888a41a1665533150542edfd05e545521566
BRAYAN DANIEL ARENAS GIL <bdarenas1@misena.edu.co>
2023-09-20 17:06:14 -0500

    ojala funcione

--- principal/views.py
+++ principal/views.py
@@ -93,4 +93,15 @@ class LibroViewSet(viewsets.ModelViewSet): #Se crea funcion para la api, con sol
 	queryset = Libro.objects.all()  #Colocamos como variable ya preescrita segun la documentacion y que nos mande la lista de todos los libros
 	serializer_class = LibroSerializer #Serializer nos ayudara a colocor todo en json
 
-	
+	def get_queryset(self):
+		genero = self.request.query_params.get('genero')
+		fecha_inicio = self.request.query_params.get('fecha_inicio')
+		fecha_fin = self.request.query_params.get('fecha_fin')
+		
+		if genero and fecha_inicio and fecha_fin:
+			filtro = Libro.objects.filter(
+            genero=genero,
+            publicacion__range=(fecha_inicio, fecha_fin)
+			)			
+			
+		return filtro
